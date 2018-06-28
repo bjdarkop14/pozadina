ec64feaf804572d6a513eaa6930ad0f14f23cdda
bjdarkop14 <karatedaki@gmail.com>
2018-06-28 15:39:45 +0200

    Kretanje

--- Kretanje.py
+++ Kretanje.py
@@ -1,32 +1,29 @@
 import math
-import pygame
-import numpy
-from Unos_podataka import *
-from pygame.locals import *
-import math
 
 playerX = 194
 playerY = 375
-def Brzina(brzinaStara, Gas, Kocnica, X0x, X0y):
-    if X0x == playerX & X0y == playerY:
+def Brzina(brzinaStara, Gas, Kocnica, X0x,X0y):
+    if X0x == playerX and X0y == playerY:
         brzina = (Gas - Kocnica)
         return brzina
     else:
         brzina=brzinaStara+(Gas-Kocnica)
+        if brzina<0:
+            brzina = 0
         return brzina
 
 
+def Sledeca_Pozicija(Volan, brzina, X0x, X0y):
+    if Volan < 0:
+        Xx = X0x - brzina * math.cos(Volan)
+        Xy = X0y -  brzina* math.sin(Volan)
+    elif Volan == 0:
+        Xx = X0x
+        Xy = X0y
+    elif Volan > 0:
+        Xx = brzina*math.cos(Volan) + X0x
+        Xy = X0x - brzina*math.sin(Volan)
 
+    return Xx,Xy
 
-    # if Volan < 0:
-    #     Ax = X0x - int((Gas - Kocnica)*math.cos(Volan))
-    #     Ay = X0y - abs(int((Gas - Kocnica) * math.sin(Volan)))
-    # if Kocnica > Gas:
-    #
-    # elif Volan > 0:
-    #     Xx = int((Gas - Kocnica)*math.cos(Volan)) + X0x
-    #     Xy = X0x - abs(int((Gas - Kocnica)*math.sin(Volan)))
-    #
-    # Xx = X0x
-    # Xy = X0y
 
--- main.py
+++ main.py
@@ -5,35 +5,40 @@ import time
 import math
 pygame.init()
 
+# clock = pygame.time.Clock() # load clock
+
 f = open("podaci.txt", "w")
 f.write('')
 button = pygame.Rect(160, 450, 140, 32)
 
+
+
 def main():
     done = False
+    brzinaStara = 0
+    start_x = 170  # x kordinata pocetnog bloka
+    start_y = 360  # y kordinata pocetnog bloka
+    x_size = 50  # duzina bloka
+    y_size = 30
+    x = start_x
+    y = start_y
+    playerX = 194
+    playerY = 375
+    FrameRate = 60
+    dt = 1.0 / FrameRate
     while not done:
+        #clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-            start_x = 170  # x kordinata pocetnog bloka
-            start_y = 360  # y kordinata pocetnog bloka
-            x_size = 50  # duzina bloka
-            y_size = 30
-            brzinaStara = 0
-            x = start_x
-            y = start_y
-            playerX = 194
-            playerY = 375
-            FrameRate = 60
-            dt = 1.0 / FrameRate
             Crtaj(x, y, x_size, y_size)
-
             # dugme
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # 1 is the left mouse button, 2 is middle, 3 is right.
                 if event.button == 1:
                     # `event.pos` is the mouse position.
                     if button.collidepoint(event.pos):
+
                         gas = input("Unesi gas: ")
                         print(gas)
                         Gas = float(gas)
@@ -45,16 +50,18 @@ def main():
                         VolanStepen = float(volan)
                         VolanRadian = math.radians(VolanStepen)
 
-                        brzina = Brzina(brzinaStara, Gas, Kocnica, playerX, playerY)
 
-                        Xx = playerX + brzina * dt
+                        brzina = Brzina(brzinaStara, Gas, Kocnica, playerX, playerY)
+                        Xx, Xy = Sledeca_Pozicija(VolanRadian,brzina,playerX,playerY)
                         playerX = Xx
-                        Xy = playerY + brzina * dt
                         playerY = Xy
+
                         f.write((gas + ", " + kocnica + ", " + volan + "\t" + str(brzina) + ", " + str(brzinaStara) + ", " + str(float(playerX)) + ", " + str(float(playerY)) + ", " + str(float(dt)) + "\n"))
                         print((gas + ", " + kocnica + ", " + volan + "\t" + str(brzina) + ", " + str(brzinaStara) + ", " + str(float(playerX)) + ", " + str(float(playerY)) + ", " + str(float(dt)) + "\n"))
-                        brzinaStara=brzina
 
+                        print(brzinaStara)
+                        brzinaStara=brzina
+                        print(brzinaStara)
 if __name__ == '__main__':
         main()
         pygame.quit()
