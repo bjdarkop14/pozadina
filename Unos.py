8d254f2a32f3a2d7058a3f57a70acfb6624cf896
bjdarkop14 <karatedaki@gmail.com>
2018-06-29 22:40:05 +0200

    Simulacija gotova

--- Unos.py
+++ Unos.py
@@ -3,7 +3,7 @@ from Kretanje import *
 
 def Unos(instrukcija, brzinaStaraX,brzinaStaraY,playerX, playerY):
     f = open("podaci.txt", "a")
-    dt = 10
+    dt = 0.1
     Gas = instrukcija[0]
     Kocnica = instrukcija[1]
     VolanStepen = instrukcija[2]
