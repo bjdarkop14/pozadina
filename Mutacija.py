6acd47c644eecb5393cb5023d0c580caddab93f4
bjdarkop14 <karatedaki@gmail.com>
2018-07-09 16:09:39 +0200

    Promenjena Mutacija

--- Mutacija.py
+++ Mutacija.py
@@ -1,7 +1,7 @@
 import random
 def Mutacija(Niz):
     for i in range(0,24):
-        Niz[i][0] = Niz[i][0] + random.randint(-20,20)
-        Niz[i][1] = Niz[i][1] + random.randint(-20,20)
-        Niz[i][2] = Niz[i][2] + random.randint(-30,30)
+        Niz[i][0] = Niz[i][0] + random.randint(-30,30)
+        Niz[i][1] = Niz[i][1] + random.randint(-30,30)
+        Niz[i][2] = Niz[i][2] + random.randint(-25,25)
     return Niz
